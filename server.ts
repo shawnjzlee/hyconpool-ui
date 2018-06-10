c227558e7f9d17cd1a4eb33ae3ce4166d8b77355
Shawn Lee <shawn@glosfer.com>
2018-06-10 17:23:52 +0900

    Creating rest server

--- server.ts
+++ server.ts
@@ -1,33 +1,56 @@
 import * as bodyParser from "body-parser"
 import * as express from "express"
-import * as http from "http"
-// import * as https from "https"
 import * as mysql from "mysql"
 import { dbConfig } from "./dbConfig"
 import { RestServer } from "./restServer"
 
-const db = mysql.createConnection(dbConfig)
-const app = express()
+export class SQLServer {
+    private app: express.Application
+    private db: mysql.Connection
+    private rest: RestServer
 
-function restRoutes(expressApp: express.Express, dbIn: mysql.Connection) {
-    expressApp.use(bodyParser.json())
-    let router: express.Router
-    const rest = new RestServer(dbIn)
-    router = express.Router()
+    constructor(port: number = 8080, options: any) {
+        this.app = express()
+        this.db = mysql.createConnection(dbConfig)
 
-    router.get("/address", async (req: express.Request, res: express.Response) => {
-        res.json(await rest.getAddress(req.body.address))
-    })
+        this.app.use(bodyParser.json())
+        this.app.use(express.static("./build"))
+        this.app.use(express.static("node_modules"))
+        this.restRoutes()
+        this.init(port)
+        this.rest = new RestServer(this.db)
+        this.app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+            res.status(404)
+            res.json({
+                status: 404
+            })
+        })
 
-    router.get("/payout", async (req: express.Request, res: express.Response) => {
-        res.json(await rest.getPayout(req.body.address))
-    })
+    }
 
-    expressApp.use(router)
+    public async init(port: number) {
+        await new Promise((resolve, reject) => {
+            this.db.connect((err) => {
+                if (err) {
+                    reject(err)
+                } resolve()
+            })
+        })
+        
+        return new Promise((resolve, reject) => {
+            this.app.listen(port, () => resolve())
+        })
+    }
+    public restRoutes() {
+        let router: express.Router
+        router = express.Router()
+
+        router.get("/address", async (req: express.Request, res: express.Response) => {
+            res.json(await this.rest.getAddress(req.body.address))
+        })
+
+        router.get("/payout", async (req: express.Request, res: express.Response) => {
+            res.json(await this.rest.getPayout(req.body.address))
+        })
+    }
 }
-
-const staticFiles = app.use(express.static("./build"))
-http.createServer(staticFiles).listen(80, () => {
-    console.log("Server listening on: http://localhost:8080")
-    restRoutes(app, db)
-})
