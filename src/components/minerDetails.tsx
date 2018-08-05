9cd289b515a3f034735e232e3758f6240448ffc5
Josiah Evans <koreanhalo@gmail.com>
2018-08-05 22:18:53 +0900

    fix bug that required you to mine for an hour before seeing data

--- src/components/minerDetails.tsx
+++ src/components/minerDetails.tsx
@@ -285,14 +285,16 @@ export class MinerDetails extends Component<IMinerProps, IMinerDetailsState> {
             minerPayouts.push(payout)
         }
 
-        const dataSample = response.minerData.slice(response.minerData.length - 7, response.minerData.length - 1)
+        const sampleStart = Math.min(response.minerData.length, 7)
+        const sampleEnd = Math.max(response.minerData.length - 1, 1)
+        const dataSample = response.minerData.slice(response.minerData.length - sampleStart, sampleEnd)
         let totalHashes = 0
         for (const row of dataSample) {
             totalHashes = totalHashes + Number(row.valid_hashes) + Number(row.pending_hashes) + Number(row.stale_hashes)
         }
 
-        const timeBegin = this.timestampToSeconds(response.minerData[response.minerData.length - 7].timestamp)
-        const timeEnd = this.timestampToSeconds(response.minerData[response.minerData.length - 1].timestamp)
+        const timeBegin = this.timestampToSeconds(response.minerData[response.minerData.length - sampleStart].timestamp)
+        const timeEnd = this.timestampToSeconds(response.minerData[sampleEnd].timestamp)
 
         this.setState({ hashrate: (totalHashes / Math.abs(timeBegin - timeEnd)).toFixed(4) })
         this.setState({ currentFee: (minerFee * 100).toFixed(3) })
